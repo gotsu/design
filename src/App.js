9639c61495a382ad08c1c8a5d31abe0bf2fc8574
ywnwalone <ywnwalone@naver.com>
2018-05-07 02:11:18 +0900

    eject를 위한 commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { IconBookmark } from 'react-gotsu-icons';
+import { IconBookmark, IconPencil } from 'react-gotsu-icons';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
-          <IconBookmark width="64px" fill="skyblue" />
+          <IconBookmark width="40px" fill="skyblue" />
+          <IconPencil width=" 40px" fill="pink" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
