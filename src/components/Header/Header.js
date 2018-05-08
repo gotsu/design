a5704ca2220007cbfc4000bc5c387ca9c7ebdff5
ywnwalone <ywnwalone@naver.com>
2018-05-08 22:33:03 +0900

    디자인 조금 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ReactLogo from 'Logo.svg';
 import styles from './Header.scss';
 import classNames from 'classnames/bind';
 
@@ -8,6 +9,8 @@ const cx = classNames.bind(styles);
 
 const Header = () => (
   <div className={cx('Header')}>
+    {/* <ReactLogo /> */}
+    <img src={ReactLogo} alt="img" style={{ width: '60px', lineHeight: '60px', height: '60px', marginLeft: '2rem' }} />
     <NavLink className={cx('title')} to="/icons">React Gotsu Icons</NavLink>
     <ul>
       <li>
@@ -20,6 +23,7 @@ const Header = () => (
         <NavLink activeClassName={cx('active')} to="/icons/tutorial">Tutorial</NavLink>
       </li>
     </ul>
+    <a href="https://github.com/gotsu/icons/" target="_blank" rel="noopener noreferrer" className={cx('hreflink')} >GitHub </a>
   </div>
 );
 
