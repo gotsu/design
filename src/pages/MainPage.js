4199b2cebecd2aa6f39e298f79f3dbc0a3cfb86e
ywnwalone <ywnwalone@naver.com>
2018-05-08 19:44:36 +0900

    import merge

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Header from 'components/Header';
+import IconBox from 'components/IconBox';
+import * as icons from 'react-gotsu-icons';
 
 const MainPage = () => {
+
+  let test = () => {
+    console.log(icons);
+    let str = '';
+    for (let item in icons) {
+      console.log(item)
+      str += <IconBox icon={(<item fill="white" />)} name={item} />
+    }
+    return str;
+  }
+  test()
   return (
     <div>
       <Header />
       <section>
-        <h2 style={{ padding: '2rem 0' }}>
+        <h2 onClick={test} style={{ padding: '2rem 0' }}>
           We are developing this place...
         </h2>
       </section>
